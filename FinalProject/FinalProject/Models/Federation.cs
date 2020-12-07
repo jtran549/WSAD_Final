7c091a01269bc84a5b535174dc1112a865f8bf0e
Jimmy Tran <jtran549@yahoo.com>
2020-12-06 20:49:12 -0500

    Created pages and added federation object

---
+++ FinalProject/FinalProject/Models/Federation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Federation
+    {
+        public string Name { get; set; }
+        public string WeighIn { get; set; }
+        public string Tested { get; set; }
+        public double Cost { get; set; }
+
+    }
+}
