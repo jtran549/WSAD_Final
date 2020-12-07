1c3d0a57a6b1068e1f764b7dd7e7724f7b98ab90
Jimmy Tran <jtran549@yahoo.com>
2020-12-06 22:21:36 -0500

    Added repository dependency injection into Federation controller

--- FinalProject/FinalProject/Controllers/FederationsController.cs
+++ FinalProject/FinalProject/Controllers/FederationsController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassSchedule.Models;
+using FinalProject.Models;
+using FinalProject.Models.DataLayer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
 {
     public class FederationsController : Controller
     {
+        private Repository<Federation> federations { get; set; }
+        public FederationsController(FederationContext ctx) => federations = new Repository<Federation>(ctx);
         public IActionResult Index()
         {
             return View();
